074a21bf9227660ab16859207018295eafae86ff
Anushka Uppula <ANUSHKACHARY@GMAIL.COM>
2025-04-24 18:14:13 -0500

    dogmanagementsystem code

---
+++ src/main/java/com/anuclass/DMS/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.anuclass.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anuclass.DMS.Models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
